bc89009a71c8bf103de2f2b4a74dd2084fbc951a
Aakriti Kedia <akedia614@gmail.com>
2023-03-14 21:18:33 -0700

    graph updated

--- wordle_bot/evaluation_bot/evaluate.py
+++ wordle_bot/evaluation_bot/evaluate.py
@@ -1,10 +1,14 @@
 from enum import Enum
-from wordle_bot.utils import VocabUtil
-from wordle_bot.evaluation_bot.utils import EvaluationUtils
-
 import time
 import matplotlib.pyplot as plt
+
 from wordle_bot.utils import VocabUtil
+from utils import EvaluationUtils
+
+# Global constants
+DATA_FOLDER_PATH = "././data/"
+ALL_VOCAB_FILENAME = "five_letter_words.pickle"
+
 # from evaluation_bot.utils import EvaluationUtils
 # from wordle_bot.evaluation_bot.evaluate import EvaluationBot
 import random
@@ -21,11 +25,11 @@ class EvaluationBot:
         vocalUtil = VocabUtil()
         self.corpus = vocalUtil.read_valid_vocab(ALL_VOCAB_FILENAME, DATA_FOLDER_PATH)
 
-    def invalid_word_response():
+    def invalid_word_response(self):
         return [], False
 
     def brute_force(self, guess_word, correct_word):
-
+        valid_word = False
         for word in self.corpus:
             if word == guess_word:
                 valid_word = True
@@ -35,7 +39,7 @@ class EvaluationBot:
 
         return self.get_letter_colours(guess_word, correct_word)
 
-    def get_letter_colours():
+    def get_letter_colours(self, guess_word, correct_word):
         letter_colours = []
 
         guess_word = guess_word.lower()
@@ -64,33 +68,38 @@ class EvaluationBot:
             trie.insert_word(word)
         return trie
         
-    def is_valid_word(self, word, corpus):
-        trie = self.create_trie(corpus)
+    def is_valid_word(self, word):
+        trie = self.create_trie(self.corpus)
         return trie.search_word(word)
 
 
     # function to create a time graph for comparing brute_force and evaluate_guessed_word_optimal
 
-    def create_time_graph(self, ):
-        evaluationBot = EvaluationBot("wordle_bot/data", "all_vocab.txt")
+    def create_time_graph(self):
+        print("creating time graph")
 
         time_brute_force = []
         time_optimal = []
-        for iterations in range(N_ITERATIONS):
-            correct_word = EvaluationUtils.select_correct_word()
-            guess_word =EvaluationUtils.generate_random_word(self.corpus)
+        brute_force_time_current, time_optimal_current = 0, 0
+        for _ in range(N_ITERATIONS):
+            correct_word = EvaluationUtils.select_correct_word(self.corpus)
+            guess_word =EvaluationUtils.generate_random_word()
             start_time = time.time()
-            evaluationBot.brute_force(guess_word, correct_word)
+            self.brute_force(guess_word, correct_word)
             end_time = time.time()
-            time_brute_force.append(end_time - start_time)
+            brute_force_time_current += end_time - start_time
+            time_brute_force.append(brute_force_time_current)
 
             start_time = time.time()
-            evaluationBot.evaluate_guessed_word_optimal(guess_word, correct_word)
+            self.evaluate_guessed_word_optimal(guess_word, correct_word)
             end_time = time.time()
-            time_optimal.append(end_time - start_time)
+            time_optimal_current += end_time - start_time
+            time_optimal.append(time_optimal_current)
 
         plt.plot(time_brute_force, label="brute_force")
         plt.plot(time_optimal, label="optimal")
+        plt.xlabel("Number of word guesses")
+        plt.ylabel("Time taken")
         plt.legend()
         plt.show()
         print("plot created")
@@ -120,3 +129,8 @@ class Trie:
             else:
                 return False
         return root.term
+
+
+if __name__ == "__main__":
+    evaluationBot = EvaluationBot(DATA_FOLDER_PATH, ALL_VOCAB_FILENAME)
+    evaluationBot.create_time_graph()
