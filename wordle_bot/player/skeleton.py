aebdf6ed9d96c8b5b76dc2ddfb7fd4a2f562255b
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2023-03-14 21:18:39 -0700

    Player Skeleton

---
+++ wordle_bot/player/skeleton.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, Vn_ds, brute_force=False):
+        self.brute_force = brute_force
+        self.Vn_structure = Vn_ds
+        pass
+    
+    def get_next_guess(self):
+        pass
+
+    def update_game_state(self, wordle_row):
+        pass
---
+++ wordle_bot/player/strategy1.py
@@ -0,0 +1,21 @@
+
+
+class PlayerStrategy1:
+    def __init__(self, Wn, Vn_ds, brute_force=False):
+        self.brute_force = brute_force
+        self.Vn_structure = Vn_ds
+        self.Wn = Wn
+    
+    def update_game_state(self, wordle_row):
+        self.Vn_structure.update(wordle_row)
+
+    def get_next_guess(self):
+        if self.brute_force:
+            return self.get_next_guess_brute_force(game, Vn, Wn)
+        else:
+            return self.get_next_guess_optimized(game, Vn, Wn)
+        
+    def get_next_guess_brute_force(self, Wn):
+
+
+
