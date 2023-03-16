b2988714e508ba8b0df34f09dd85682c5920fd9e
karansanthosh <ksanthosh@ucsd.edu>
2023-03-15 17:15:53 -0700

    add reset for vn

--- wordle_bot/player/Vn.py
+++ wordle_bot/player/Vn.py
@@ -1,6 +1,8 @@
 from wordle_bot.utils import VocabUtil
 from skeleton import Vn_DS
 from wordle_bot.evaluation_bot import Colour
+import timeit
+import numpy as np
 
 # Global constants
 DATA_FOLDER_PATH = "././data/"
@@ -22,8 +24,8 @@ class Vn_reduction(Vn_DS):
             for i in range(5):
                 self.hashtable[i][ord(word[i]) - ord('a')].append(word)
 
-    def reset(self):
-        self.Vn = self.Wn.copy()
+    def reset(self, Vn = None):
+        self.Vn = self.Wn.copy() if Vn is None else Vn
 
     def update(self, word, colors):
         for i, letter in enumerate(word):
@@ -48,9 +50,41 @@ class Vn_reduction(Vn_DS):
                 for word in to_delete:
                     self.Vn.discard(word)
 
+    def update2(self, word, colors):
+        vn2 = self.Vn.copy()
+        for current in vn2:
+            for i, letter in enumerate(current):
+                if colors[i] == 0 and word[i] in current:
+                    self.Vn.remove(current)
+                    break
+                elif colors[i] == 1 and (word[i] not in current or letter == word[i]):
+                    self.Vn.remove(current)
+                    break
+                elif colors[i] == 2 and letter != word[i]:
+                    self.Vn.remove(current)
+                    break
+        return self.Vn
+
     def get_Vn(self):
         return self.Vn
 
 vn_red = Vn_reduction(corpus)
-vn_red.update('apple', [2, 0, 0, 1, 2])
+v =vn_red.update('slate', [0, 0, 0, 0, 0])
+v =vn_red.update('bingo', [0, 0, 0, 0, 0])
+v =vn_red.update('furzy', [0, 2, 0, 0, 2])
+v =vn_red.update('yucky', [1, 2, 0, 0, 2])
+v =vn_red.update('dumpy', [0, 2, 0, 2, 2])
+
 print(vn_red.get_Vn())
+
+def time_check1():
+    vn_red.reset()
+    vn_red.update(np.random.choice(corpus), np.random.randint(0, 3, 5))
+
+def time_check2():
+    vn_red.reset()
+    vn_red.update2(np.random.choice(corpus), np.random.randint(0, 3, 5))
+
+# print(timeit.timeit("time_check1()", setup="from __main__ import time_check1", number=1000))
+
+# print(timeit.timeit("time_check2()", setup="from __main__ import time_check2", number=1000))
